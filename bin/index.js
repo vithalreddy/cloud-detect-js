313a885eff510a1d7ab9b523e34a8c730fa5c2ff
Vithal Reddy <vithal.reddy@zylotech.com>
2019-09-05 17:35:43 +0530

    added cli

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+const commander = require('commander');
+
+const { cloudProvider } = require('../build');
+const pkg = require('../package.json');
+
+const program = new commander.Command();
+
+async function detect() {
+  const provider = await cloudProvider();
+  console.log(provider);
+}
+
+program
+  .version(pkg.version)
+  .command('detect')
+  .description("Detect's Host Machine's Cloud Provider")
+  .action(detect);
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
